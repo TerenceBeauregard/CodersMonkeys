f17f3c0a690ebb88db2a8add4043e0755b6a5b7c
LaCreme <lacreme@mbp-de-lacreme.home>
2023-04-23 16:44:54 +0200

    design system typo et button

---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBakXq_ewg2EKNQA4KQgl09sdKCqiCrH1U",
+  authDomain: "codersmonkeys.firebaseapp.com",
+  projectId: "codersmonkeys",
+  storageBucket: "codersmonkeys.appspot.com",
+  messagingSenderId: "114028945450",
+  appId: "1:114028945450:web:a84c08f34f619c6564a353"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,55 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Seo } from '@/ui/components/seo/seo'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Seo } from "@/ui/components/seo/seo";
+import { Typography } from "@/ui/design-system/typography/typography";
+import { Button } from "@/ui/design-system/button/button";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <Seo title="Coders Monkeys" description='description'/>
-      <h1>Hello word</h1>
-    </>
-  )
+	return (
+		<>
+			<Seo title="Coders Monkeys" description="description" />
+      
+			<Typography variant="display" component="h1" theme="primary" weight="medium">
+				Coders Monkeys
+			</Typography>
+      <Typography variant="h1" component="h1" theme="gray">
+				Coders Monkeys
+			</Typography>
+      <Typography variant="h2" component="h1" theme="black" weight="medium">
+				Coders Monkeys
+			</Typography>
+      <Typography variant="h3" component="h1" theme="secondary">
+				Coders Monkeys
+			</Typography>
+      <Typography variant="h4" component="h1">
+				Coders Monkeys
+			</Typography>
+      <Typography variant="h5" component="h1">
+				Coders Monkeys
+			</Typography>
+
+			<div className=" flex items-center gap-5 p-10">
+				<Button variant="accent" size="small" >Accent</Button>
+				<Button variant="secondary" size="small" >Secondary</Button>
+				<Button variant="disabled"  size="small" disabled>Disabled</Button>
+				<Button variant="outline" size="small" >Outline</Button>
+			</div>
+
+      <div className=" flex items-center gap-5 p-10">
+				<Button variant="accent">Accent</Button>
+				<Button variant="secondary">Secondary</Button>
+				<Button variant="disabled" disabled>Disabled</Button>
+				<Button variant="outline">Outline</Button>
+			</div>
+
+      <div className=" flex items-center gap-5 p-10">
+      <Button variant="accent" size="large" >Accent</Button>
+				<Button variant="secondary" size="large" >Secondary</Button>
+				<Button variant="disabled"  size="large" disabled>Disabled</Button>
+				<Button variant="outline" size="large" >Outline</Button>
+			</div>
+		</>
+	);
 }
