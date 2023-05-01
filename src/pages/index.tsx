b19ab1f9ba0b160f36733592e0ef7e2b25cce587
LaCreme <lacreme@MacBook-Pro-de-LaCreme.local>
2023-05-01 17:20:19 +0200

    spinner et logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,16 @@ import { Seo } from "@/ui/components/seo/seo";
 import { Typography } from "@/ui/design-system/typography/typography";
 import { Button } from "@/ui/design-system/button/button";
 import {RiUser3Line} from "react-icons/ri";
+import { Spinner } from "@/ui/design-system/spinner/spinner";
+import { Logo } from "@/ui/design-system/logo/logo";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
 		<>
+
+			<Logo />
 			<Seo title="Coders Monkeys" description="description" />
 
 			<Typography
@@ -86,6 +90,31 @@ export default function Home() {
 				<Button variant="ico" size="large" icon={{icon: RiUser3Line}} iconTheme="secondary"/>
 				<Button variant="ico" size="large" icon={{icon: RiUser3Line}} iconTheme="gray"/>
 			</div>
+
+			<div className=" flex items-center gap-5 p-10">
+				<Button isLoading variant="accent" size="large">
+					Accent
+				</Button>
+				<Button isLoading variant="secondary" size="large">
+					Secondary
+				</Button>
+				<Button isLoading variant="disabled" size="large" disabled>
+					Disabled
+				</Button>
+				<Button isLoading variant="outline" size="large">
+					Outline
+				</Button>
+				<Button isLoading variant="ico" size="large" icon={{icon: RiUser3Line}} iconTheme="accent"/>
+				<Button isLoading variant="ico" size="large" icon={{icon: RiUser3Line}} iconTheme="secondary"/>
+				<Button isLoading variant="ico" size="large" icon={{icon: RiUser3Line}} iconTheme="gray"/>
+			</div> 
+
+			<div className="flex items-center gap-4 p-10">
+				<Spinner size="small"/>
+				<Spinner size="medium"/>
+				<Spinner size="large"/>
+			</div>
+			
 		</>
 	);
 }
--- src/ui/design-system/button/button.tsx
+++ src/ui/design-system/button/button.tsx
@@ -1,5 +1,6 @@
 import { IconProps } from "@/types/iconProps";
 import clsx from "clsx";
+import { Spinner } from "../spinner/spinner";
 
 interface Props {
 	size?: "small" | "medium" | "large";
@@ -90,23 +91,42 @@ export const Button = ({
 		<>
 			<button
 				type="button"
-				className={clsx(variantStyle, icoSize, sizeStyle)}
+				className={clsx(
+					variantStyle,
+					icoSize,
+					sizeStyle,
+					isLoading && "cursor-wait",
+					"relative"
+				)}
 				onClick={() => console.log("click")}
 				disabled={disabled}
 			>
-				{icon && variant === "ico" ? (
-					<icon.icon size={icoSize} />
-				) : (
-					<div className={clsx(icon && "flex items-center gap-1")}>
-						{icon && iconPosition === "left" && (
-							<icon.icon size={icoSize} />
-						)}
-						{children}
-						{icon && iconPosition === "right" && (
-							<icon.icon size={icoSize} />
+				{isLoading && (
+					<div className="absolute inset-0 flex items-center justify-center">
+						{variant === "accent"  || variant === "ico"? (
+							<Spinner size="small" variant="white" />
+						) : (
+							<Spinner size="small" />
 						)}
 					</div>
 				)}
+				<div className={clsx(isLoading && "invisible")}>
+					{icon && variant === "ico" ? (
+						<icon.icon size={icoSize} />
+					) : (
+						<div
+							className={clsx(icon && "flex items-center gap-1")}
+						>
+							{icon && iconPosition === "left" && (
+								<icon.icon size={icoSize} />
+							)}
+							{children}
+							{icon && iconPosition === "right" && (
+								<icon.icon size={icoSize} />
+							)}
+						</div>
+					)}
+				</div>
 			</button>
 		</>
 	);
